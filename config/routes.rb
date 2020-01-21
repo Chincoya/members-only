33206b157729c0a65979a0d47acbc810beb1f3ca
Daniel Chincoya <chincoya@ciencias.unam.mx>
2020-01-21 10:17:17 -0600

    Fix conflicts?

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # get 'users/new'
   resources :users, only: [:new, :show, :create]
   resources :post, only: [:new, :create, :index], :controller => 'comments'
-
   root 'static_pages#home'
+
+  #get '/index', to: 'comments#index' 
+  #get 'comments/new', to: 'comments#new'
+  #post 'comments', to: 'comments#create'
   get '/login', to: 'session#new'
   post '/login', to: 'session#create'
   delete '/logout', to: 'session#destroy'
