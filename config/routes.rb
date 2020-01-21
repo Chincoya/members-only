6d35c4755f8027f97f79dd5371ef39364080a3bb
Daniel Chincoya <chincoya@ciencias.unam.mx>
2020-01-21 10:15:25 -0600

    Changed routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   # get 'users/new'
   resources :users, only: [:new, :show, :create]
-  root 'static_pages#home'
+  resources :post, only: [:new, :create, :index], :controller => 'comments'
 
-  get '/index', to: 'comments#index' 
-  get 'comments/new', to: 'comments#new'
-  post 'comments', to: 'comments#create'
+  root 'static_pages#home'
   get '/login', to: 'session#new'
   post '/login', to: 'session#create'
   delete '/logout', to: 'session#destroy'
